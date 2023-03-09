59cac8ff40720b9bfea629d7e9ca2f91c439ecdb
awatekomal <awatekomal12354@gmail.com>
2023-03-09 17:55:54 +0530

    Initial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
+import {ListingsPageComponent} from "./listings-page/listings-page.component";
+import {ContactPageComponent} from "./contact-page/contact-page.component";
+import {EditListingPageComponent} from "./edit-listing-page/edit-listing-page.component";
+import {MyListingPageComponent} from "./my-listing-page/my-listing-page.component";
+import {NewListingPageComponent} from "./new-listing-page/new-listing-page.component"
+
+
+
+const routes: Routes = [
+  {path:'', redirectTo:'listings',pathMatch:'full'},
+  {path:'listings',component:ListingsPageComponent,pathMatch:'full'},
+  {path:'listings/:id' ,component:ListingDetailPageComponent},
+  {path:'contact/:id',component:ContactPageComponent},
+  {path:'edit-listing/:id',component:EditListingPageComponent},
+  {path:'my-listings',component:MyListingPageComponent},
+  {path:'new-listings',component:NewListingPageComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/my-listing-page/my-listing-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-listing-page',
+  templateUrl: './my-listing-page.component.html',
+  styleUrls: ['./my-listing-page.component.css']
+})
+export class MyListingPageComponent {
+
+}
